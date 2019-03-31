fc2535c4c023d1dfaf165e123738f1ffb1e9e764
Andy Young <ayscript@gmail.com>
2019-03-31 11:08:48 -0600

    Set up wcw-020

---
+++ JS-Building-Blocks/wcw-020-fashionInventory.js
@@ -0,0 +1,50 @@
+/*
+The task now is to find all the laced shoes.
+Output shoe names that contain“ lace” in them, and indicate which word contains“ lace”.
+
+The output format should be structured like this: [{
+    "nameWords": ["tasselled", "black", "low-top", "lace-up"],
+    "targetWordIndex": 3
+}, {
+    "nameWords": ["tasselled", "green", "low-top", "lace-up"],
+    "targetWordIndex": 3
+}, {
+    "nameWords": ["red", "leather", "laced", "sneakers"],
+    "targetWordIndex": 2
+}, {
+    "nameWords": ["black", "leather", "laced", "sneakers"],
+    "targetWordIndex": 2
+}]
+*/
+
+var inventory = [{
+    name: 'Brunello Cucinelli',
+    shoes: [{
+        name: 'tasselled black low-top lace-up',
+        price: 1000
+    }, {
+        name: 'tasselled green low-top lace-up',
+        price: 1100
+    }, {
+        name: 'plain beige suede moccasin',
+        price: 950
+    }, {
+        name: 'plain olive suede moccasin',
+        price: 1050
+    }]
+}, {
+    name: 'Gucci',
+    shoes: [{
+        name: 'red leather laced sneakers',
+        price: 800
+    }, {
+        name: 'black leather laced sneakers',
+        price: 900
+    }]
+}];
+
+function getLace(data) {
+	// answer
+}
+
+getLace(inventory);
