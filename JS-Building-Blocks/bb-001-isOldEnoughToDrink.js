f445798d0b3dc93a07da4ab46b0e5d596f5ad584
Andy Young <ayscript@gmail.com>
2019-03-31 10:25:25 -0600

    complete bb-001

---
+++ JS-Building-Blocks/bb-001-isOldEnoughToDrink.js
@@ -0,0 +1,7 @@
+// Create fxn, taking age (int), return bool is legally
+// allowed to be served
+
+const isOldEnoughToDrink = age => age > 20;
+
+// isOldEnoughToDrink(20) --> false
+// isOldEnoughToDrink(21) --> true
